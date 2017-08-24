da0c18c5dc070603d6da419d95cdab729eed9a0c
ggnohc <gheegnohc@gmail.com>
2017-08-24 16:03:53 +0800

    Ommit previous setting of "send_pickup = True" as causing Rover not moving

--- drive_rover.py
+++ drive_rover.py
@@ -74,9 +74,8 @@ class RoverState():
         self.samples_to_find = 0 # To store the initial count of samples
         self.samples_located = 0 # To store number of samples located on map
         self.samples_collected = 0 # To count the number of samples collected
-        self.near_sample = 0 # Will be set to telemetry value data["near_sample"]
         self.picking_up = 0 # Will be set to telemetry value data["picking_up"]
-        self.send_pickup = True # Set to True to trigger rock pickup
+        self.send_pickup = False # Set to True to trigger rock pickup
 # Initialize our rover
 Rover = RoverState()
 
