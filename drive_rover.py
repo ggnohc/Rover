2200ba9b9815dcd613bf950d410418fe7231f92b
ggnohc <gheegnohc@gmail.com>
2017-08-28 22:26:18 +0800

    Added self.snap to track position at particular time to check if the rover is stuck.

--- drive_rover.py
+++ drive_rover.py
@@ -40,6 +40,7 @@ class RoverState():
     def __init__(self):
         self.start_time = None # To record the start time of navigation
         self.total_time = None # To record total duration of naviagation
+        self.snap = (0,0,0,0) # (x,y,yaw,time), snap shot for position/yaw in particular time
         self.img = None # Current camera image
         self.pos = None # Current position (x, y)
         self.yaw = None # Current yaw angle
